858a36d647bfd557165d0cc6399ac205b5edb4b5
juergen stodolka <juergen.stodolka@sealsystems.de>
2019-02-13 16:30:37 +0100

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ var app = express();
 // Configure the middleware
 app.use(bodyParser.json());
 
-// POST: Create a new todo
+// POST /todos: Create a new todo in database
 app.post('/todos', (req, res) => {
   console.log(req.body);
   var todo = new Todo({
@@ -27,6 +27,19 @@ app.post('/todos', (req, res) => {
   );
 });
 
+// GET /todos
+app.get('/todos', (req, res) => {
+  Todo.find().then(
+    (todos) => {
+      res.send({ todos });
+    },
+    (e) => {
+      res.send(400).send(e);
+    }
+  );
+});
+
+// Start the server listening on port 3000
 app.listen(3000, () => {
   console.log('Started on port 3000');
 });
