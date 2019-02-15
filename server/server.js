26d4cd6bfd348465e26b98ff066757a878f66621
juergen stodolka <juergen.stodolka@sealsystems.de>
2019-02-15 17:11:09 +0100

    Add route GET/todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
@@ -37,6 +38,30 @@ app.get('/todos', (req, res) => {
       res.send(400).send(e);
     }
   );
+});
+
+// GET /todos/123432
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    // Invalid ObjectID
+    return res.status(404).send();
+  }
+
+  // findById
+  Todo.findById(id)
+    .then((todo) => {
+      if (!todo) {
+        console.log('Id not found');
+        return res.status(404).send();
+      }
+      console.log('Todo by Id', todo);
+      res.send({ todo });
+    })
+    .catch((e) => {
+      console.log('ERROR');
+      res.status(400).send();
+    });
 });
 
 // Start the server listening on port 3000
